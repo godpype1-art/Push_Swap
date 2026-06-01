20908ca005170e738de40da09915a783e28e0780
Antonio Franco Santos <afranco-@c1r7s2.42lisboa.com>
2026-06-01 18:42:35 +0100

    start merger

---
+++ merge_sorting.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   merge_sorting.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: afranco- <afranco-@student.42lisboa.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/01 18:13:17 by afranco-          #+#    #+#             */
+/*   Updated: 2026/06/01 18:20:34 by afranco-         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void merge(t_stack *stackA, t_stack *stackB, int size)
+{
+	
+}
+
+void merge_sorting(t_stack *stackA, t_stack *stackB, int size)
+{
+	
+}
